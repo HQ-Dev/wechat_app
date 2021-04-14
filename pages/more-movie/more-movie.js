1b58c3a9ee9116d22947d1b675b30735a111bbff
HQ-Dev <hq_dev@163.com>
2021-04-14 23:24:35 +0800

    json和代码设置bartitile

--- pages/more-movie/more-movie.js
+++ pages/more-movie/more-movie.js
@@ -16,7 +16,7 @@ Page({
    */
   onLoad: function (options) {
     const movieType = options.type;
-    this._movieType = movieType;
+    this.data._movieType = movieType;
     wx.request({
       url: app.commonUrl + movieType,
       data:{
@@ -36,8 +36,26 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    wx.showNavigationBarLoading({
-      success: (res) => {},
+    
+    // wx.showNavigationBarLoading({
+    //   success: (res) => {},
+    // }),
+
+    // 根据选择的电影类型显示不同的 barTitle
+    let barTitle = "电影"
+    switch(this.data._movieType) {
+      case 'in_theaters':
+        barTitle = "正在热映"
+        break
+      case 'coming_soon':
+        barTitle = "即将上映"
+        break 
+      case 'top250':
+        barTitle = "豆瓣top250"
+        break
+    }
+    wx.setNavigationBarTitle({
+      title: barTitle,
     })
   },
 
@@ -68,7 +86,7 @@ Page({
   onPullDownRefresh: function () {
     wx.showNavigationBarLoading();
     wx.request({
-      url: app.commonUrl + this._movieType,
+      url: app.commonUrl + this.data._movieType,
       data : {
         start:0,
         count:12
@@ -89,7 +107,7 @@ Page({
   onReachBottom: function () {
     wx.showNavigationBarLoading();
     wx.request({
-      url: app.commonUrl + this._movieType,
+      url: app.commonUrl + this.data._movieType,
       data:{
         start:this.data.movies.length,
         count:12
